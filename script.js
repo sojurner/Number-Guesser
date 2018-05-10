4fb846b0261f136e6caf7ac726161ece8828f1c5
Paul Kim <pykim0591@gmail.com>
2018-05-09 21:30:16 -0600

    Structured and organized

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//------
+//---------
 //variables
-//------
+//---------
 var userNum = document.querySelector('#guessBox');
 var guessBtn = document.querySelector('#guessButton');
 var userNumDisplay = document.querySelector('#guessDisplay');
@@ -11,23 +11,32 @@ var maxiNum = document.querySelector('#maxNum');
 var numberRange = document.querySelector('#numRange')
 var clearBtn = document.querySelector('#clearButton');
 var resetBtn = document.querySelector('#resetButton');
-//var parseMin = parseInt(miniNum.value);
-//var parseMax = parseInt(maxiNum.value);
-//console.log("hello world");
 
-
-//---------
+//--------------
 //Calling Events
-//---------
-//Guess button click triggers --> enabling reset button; linking User's input guess to Display; calling function to compare user# to random# 
+//--------------
+//Parameter button click
+applyButton.addEventListener('click', function() {
+  numParaGen();
+})
+//Guess button click 
 guessBtn.addEventListener('click', function() {
-  resetBtn.disabled = false;
-  guessDisplay.innerText = userNum.value;
-  numCompare();
+  guessCompare();
 })
 
-//Parameter button click triggers --> enabling guess|clear button; linking users min|max#s as parameters for random number generator
-applyButton.addEventListener('click', function() {
+clearBtn.addEventListener('click', function() {
+  clearPage();
+})
+
+resetBtn.addEventListener('click', function() {
+  resetPage();
+})
+
+//----------------
+//Functions
+//----------------
+//enabling guess|clear button; linking users min|max#s as parameters for random number generator
+function numParaGen() {
   guessBtn.disabled = false; 
   clearBtn.disabled = false;
   parseMin = parseInt(miniNum.value, 10) || 1;
@@ -35,50 +44,42 @@ applyButton.addEventListener('click', function() {
   numberRange.innerText = 'Pick a # between ' + parseMin + ' and ' + parseMax
   randNumber = randNumGenerator()
   randNumGenerator();
-  })
-
-clearBtn.addEventListener('click', function() {
-  clearPage()
-})
-
-resetBtn.addEventListener('click', function() {
-  resetPage();
-})
-
-// Generating a Random Number
-  function randNumGenerator() {
-  //parseMin = parseInt(miniNum.value);
-  //parseMax = parseInt(maxiNum.value);
-    return Math.floor(Math.random() * parseMax - parseMin + parseMin);   
 }
-
-//Increasing Min and Max values
-function indexRange(n) {
-  parseMin = parseMin - n;
-  parseMax = parseMax + n;
-}  
-
+//Generating a Random Number
+  function randNumGenerator() {
+    return Math.floor(Math.random() * (parseMax - parseMin) + 1);   
+}
+//enabling reset button; linking User's input guess to Display; calling function to compare user# to random# 
+function guessCompare () {
+resetBtn.disabled = false;
+  guessDisplay.innerText = userNum.value;
+  numCompare();
+}
 //Comparing User's guess and random number generated
 function numCompare() {
   userGuess = parseInt(userNum.value, 10);
   if (userGuess === randNumber) {
-    randNumGenerator();
     indexRange(10);
    return feedBackDisplay.innerText = 'BOOM!';
      resetBtn.disabled = false;
+     randNumGenerator();
   } 
-  if (userGuess < parseMin || userGuess > parseMax) {
-   return feedBackDisplay.innerText = 'Sorry the number you have dialed is out of reach. Please try again';
- }
+  if (userGuess < parseMin || userGuess > parseMax || isNaN(parseInt(userNum.value))) {
+   return feedBackDisplay.innerText = 'Sorry the number (or the lack thereof) you have dialed is not valid. Please try again';
+  }
   if (userGuess > randNumber) {
     return feedBackDisplay.innerText = 'That is too high';
   } 
   if (userGuess < randNumber) {
    return feedBackDisplay.innerText = 'That is too low';
   } 
-  
 }
-
+//Increasing Min and Max values
+function indexRange(n) {
+  parseMin = parseMin - n;
+  parseMax = parseMax + n;
+}  
+//Clearing 
 function clearPage() {
   userNumDisplay.innerText = '';
   feedBackDisplay.innerText = '';
@@ -88,14 +89,12 @@ function clearPage() {
   userNum.value = '';
   userNum.value = null;
 }
-  
+//Reloads page
 function resetPage() {
  location.reload();
 }
 
 
-
-//clearing functions
 
 
 
